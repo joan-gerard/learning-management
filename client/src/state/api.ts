7645b6e16a000b04d129c3b29c1245d2f7d06f26
Joan Gerard <joan.gerard@outlook.com>
2025-02-13 18:14:57 +0100

    clear some eslint errors

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { FetchArgs, BaseQueryApi } from "@reduxjs/toolkit/query/react";
 
